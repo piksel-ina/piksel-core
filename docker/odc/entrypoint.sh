af494943e4d847a063c05b9295407ab8ab00e6a1
taufik-shf <taufik.shf@gmail.com>
2025-10-28 07:55:18 +0000

    Update odc dockerfile to use datacube 1.9.10 and use uv to manage dependencies

--- docker/odc/entrypoint.sh
+++ docker/odc/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-# Generate config from template using environment variables
-envsubst < /home/appuser/.datacube.conf.template > /home/appuser/.datacube.conf
+# Generate datacube.conf from template with environment variables
+if [ -f /home/odc/.datacube.conf.template ]; then
+    envsubst < /home/odc/.datacube.conf.template > /home/odc/.datacube.conf
+    echo "Generated datacube.conf from environment variables"
+fi
 
 exec "$@"
