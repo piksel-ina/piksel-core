a512c0530a24de56c6fc13e9d21c1cd5e4809f3f
taufik-shf <taufik.shf@gmail.com>
2025-11-18 05:48:02 +0000

    Fix environment issues on jupyter-dev

--- docker/jupyter-dev/entrypoint.sh
+++ docker/jupyter-dev/entrypoint.sh
@@ -39,8 +39,22 @@ cat > /home/jovyan/.jupyter/lab/user-settings/@jupyterlab/docmanager-extension/p
 }
 EOF
 
+if [ -f "/home/jovyan/.venv/bin/activate" ] && [ -z "$VIRTUAL_ENV" ]; then
+    source /home/jovyan/.venv/bin/activate
+fi
+
+echo " --- Environment ---"
+echo "PATH: $PATH"
+echo "which jupyter-lab: $(which jupyter-lab || echo 'NOT FOUND')"
+echo "which jupyterhub-singleuser: $(which jupyterhub-singleuser || echo 'NOT FOUND')"
+echo "Command: $@"
+echo " -------------------"
+echo ""
+echo " ---- Versions -----"
+echo "Versions:"
 echo "Python: $(python3 --version)"
 echo "UV: $(uv --version)"
 echo "JupyterLab: $(jupyter-lab --version)"
+echo " -------------------"
 
 exec "$@"
