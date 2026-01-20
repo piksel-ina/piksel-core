738fee74300a94cd7906a39fd4a13a5affde0d02
taufik-shf <taufik.shf@gmail.com>
2026-01-20 10:09:54 +0000

    Fix some issue and  add dask,psql-client to jupyter dev image

--- docker/jupyter-dev/entrypoint.sh
+++ docker/jupyter-dev/entrypoint.sh
@@ -15,4 +15,12 @@ echo "UV: $(uv --version)"
 echo "JupyterLab: $(jupyter-lab --version)"
 echo " -------------------"
 
+if [ -n "$JUPYTERHUB_USER" ]; then
+    echo "JupyterHub environment: $JUPYTERHUB_USER"
+    export DASK_DISTRIBUTED__DASHBOARD__LINK="/user/${JUPYTERHUB_USER}/proxy/{port}/status"
+else
+    echo "Local environment"
+    export DASK_DISTRIBUTED__DASHBOARD__LINK="/proxy/{port}/status"
+fi
+
 exec "$@"
